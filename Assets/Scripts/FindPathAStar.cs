6b0085736d8ae2bd758b4527eda94e5472c0a148
SUMAN SINGHA <sumansingha047@gmail.com>
2024-05-27 21:56:57 +0530

    Start Coding

---
+++ Assets/Scripts/FindPathAStar.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class PathMarker
+{
+    public MapLocation location;
+    public float G;
+    public float H;
+    public float F;
+    public GameObject marker;
+    public PathMarker parent;
+
+    public PathMarker(MapLocation l, float g, float h, float f, GameObject marker, PathMarker p)
+    {
+        location = l;
+        G = g;
+        H = h;
+        F = f;
+        this.marker = marker;
+        parent = p;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+        {
+            return false;
+        }
+        else 
+        {
+            return location.Equals(((PathMarker) obj).location);
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        return 0;
+    }
+}
+
+
+public class FindPathAStar : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
